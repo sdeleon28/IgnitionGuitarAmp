715796a9bb37ff99a82d24c3341401b7146b1259
Santiago de Leon <sdeleon28@gmail.com>
2021-01-16 12:26:12 -0300

    First clumsy attempt at establishing screen layout

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,15 +1,6 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
-#define SIZE_FACTOR 0.5f
-#define SEPARATOR_PINK 20
-#define SEPARATOR_GREEN 40
-#define SEPARATOR_BLUE 88
-#define COLOUR_BLACK Colour(28, 28, 28)
-#define COLOUR_YELLOW Colour(249, 226, 85)
-#define COLOUR_LIGHT_GREY Colour(142, 142, 142)
-#define COLOUR_DARK_GREY Colour(58, 58, 58)
-
 using SliderAttachment = AudioProcessorValueTreeState::SliderAttachment;
 
 //==============================================================================
@@ -17,6 +8,95 @@ ShittyAmpAudioProcessorEditor::ShittyAmpAudioProcessorEditor(ShittyAmpAudioProce
     : AudioProcessorEditor(&p)
     , audioProcessor(p)
 {
+
+    const float borderSize = 3;
+    const float separatorPink = SEPARATOR_PINK * SIZE_FACTOR;
+    const float separatorBlue = SEPARATOR_BLUE * SIZE_FACTOR;
+    const float separatorGreen = SEPARATOR_GREEN * SIZE_FACTOR;
+    const float yellowBoxWidth = (504 + 2 * borderSize) * SIZE_FACTOR;
+
+    topYellowBox = {
+        .left = 2 * separatorPink,
+        .top = 2 * separatorPink,
+        .width = yellowBoxWidth,
+        .height = 58 * SIZE_FACTOR,
+        .cornerSize = 10,
+        .borderThickness = 3,
+        .colour = COLOUR_YELLOW,
+        .borderColour = COLOUR_BLACK,
+    };
+    midYellowBox = {
+        .left = 2 * separatorPink,
+        .top = topYellowBox.getBottom() + separatorPink,
+        .width = yellowBoxWidth,
+        .height = 162 * SIZE_FACTOR,
+        .cornerSize = 10,
+        .borderThickness = 3,
+        .colour = COLOUR_YELLOW,
+        .borderColour = COLOUR_BLACK,
+    };
+    topGreyBox = {
+        .left = separatorPink,
+        .top = separatorPink,
+        .width = yellowBoxWidth + 2 * separatorPink,
+        .height = topYellowBox.height + midYellowBox.height + 3 * separatorPink,
+        .colour = COLOUR_LIGHT_GREY,
+    };
+    topBlackBox = {
+        .left = 0,
+        .top = 0,
+        .width = topGreyBox.width + 2 * separatorPink,
+        .height = topGreyBox.height + 2 * separatorPink,
+        .cornerSize = 10,
+        .colour = COLOUR_BLACK,
+    };
+    bottomYellowBox = {
+        .left = 2 * separatorPink,
+        .top = topBlackBox.getBottom() + 2 * separatorPink,
+        .width = yellowBoxWidth,
+        .height = 240 * SIZE_FACTOR,
+        .cornerSize = 10,
+        .borderThickness = 3,
+        .colour = COLOUR_YELLOW,
+        .borderColour = COLOUR_BLACK,
+    };
+    bottomGreyBox = {
+        .left = separatorPink,
+        .top = topBlackBox.getBottom() + separatorPink,
+        .width = yellowBoxWidth + 2 * separatorPink,
+        .height = bottomYellowBox.height + 2 * separatorPink,
+        .colour = COLOUR_LIGHT_GREY,
+    };
+    bottomBlackBox = {
+        .left = 0,
+        .top = topBlackBox.getBottom(),
+        .width = topGreyBox.width + 2 * separatorPink,
+        .height = topGreyBox.height + 2 * separatorPink,
+        .cornerSize = 10,
+        .colour = COLOUR_BLACK,
+    };
+    gainBox = {
+        .left = midYellowBox.left + separatorBlue + borderSize,
+        .top = midYellowBox.top,
+        .width = 80 * SIZE_FACTOR,
+        .height = midYellowBox.height,
+        .colour = COLOUR_DARK_GREY,
+    };
+    toneBox = {
+        .left = gainBox.getRight() + separatorGreen,
+        .top = gainBox.top,
+        .width = gainBox.width,
+        .height = gainBox.height,
+        .colour = COLOUR_DARK_GREY,
+    };
+    levelBox = {
+        .left = toneBox.getRight() + separatorGreen,
+        .top = toneBox.top,
+        .width = toneBox.width,
+        .height = toneBox.height,
+        .colour = COLOUR_DARK_GREY,
+    };
+
     gainLabel.setText(GAIN_NAME, dontSendNotification);
     gainLabel.attachToComponent(&gainSlider, false);
     gainValue = std::make_unique<SliderAttachment>(audioProcessor.treeState, GAIN_ID, gainSlider);
@@ -62,27 +142,6 @@ ShittyAmpAudioProcessorEditor::~ShittyAmpAudioProcessorEditor()
     outLevelSlider.setLookAndFeel(nullptr);
 }
 
-struct Box {
-    float top;
-    float left;
-    float height;
-    float width;
-    float cornerSize = 0;
-    float borderThickness = 0;
-    Colour colour;
-    Colour borderColour = COLOUR_BLACK;
-
-    float getBottom()
-    {
-        return top + height;
-    }
-    
-    float getRight()
-    {
-        return left + width;
-    }
-};
-
 void paintBox(Box &box, Graphics &g)
 {
     g.setColour(box.colour);
@@ -112,104 +171,16 @@ void ShittyAmpAudioProcessorEditor::paint(Graphics &g)
 {
     g.fillAll(getLookAndFeel().findColour(ResizableWindow::backgroundColourId));
 
-    const float borderSize = 3;
-    const float separatorPink = SEPARATOR_PINK * SIZE_FACTOR;
-    const float separatorBlue = SEPARATOR_BLUE * SIZE_FACTOR;
-    const float separatorGreen = SEPARATOR_GREEN * SIZE_FACTOR;
-    const float yellowBoxWidth = (504 + 2 * borderSize) * SIZE_FACTOR;
-
-    Box topYellowBox = {
-        .left = 2 * separatorPink,
-        .top = 2 * separatorPink,
-        .width = yellowBoxWidth,
-        .height = 58 * SIZE_FACTOR,
-        .cornerSize = 10,
-        .borderThickness = 3,
-        .colour = COLOUR_YELLOW,
-        .borderColour = COLOUR_BLACK,
-    };
-    Box midYellowBox = {
-        .left = 2 * separatorPink,
-        .top = topYellowBox.getBottom() + separatorPink,
-        .width = yellowBoxWidth,
-        .height = 162 * SIZE_FACTOR,
-        .cornerSize = 10,
-        .borderThickness = 3,
-        .colour = COLOUR_YELLOW,
-        .borderColour = COLOUR_BLACK,
-    };
-    Box topGreyBox = {
-        .left = separatorPink,
-        .top = separatorPink,
-        .width = yellowBoxWidth + 2 * separatorPink,
-        .height = topYellowBox.height + midYellowBox.height + 3 * separatorPink,
-        .colour = COLOUR_LIGHT_GREY,
-    };
-    Box topBlackBox = {
-        .left = 0,
-        .top = 0,
-        .width = topGreyBox.width + 2 * separatorPink,
-        .height = topGreyBox.height + 2 * separatorPink,
-        .cornerSize = 10,
-        .colour = COLOUR_BLACK,
-    };
-    Box bottomYellowBox = {
-        .left = 2 * separatorPink,
-        .top = topBlackBox.getBottom() + 2 * separatorPink,
-        .width = yellowBoxWidth,
-        .height = 240 * SIZE_FACTOR,
-        .cornerSize = 10,
-        .borderThickness = 3,
-        .colour = COLOUR_YELLOW,
-        .borderColour = COLOUR_BLACK,
-    };
-    Box bottomGreyBox = {
-        .left = separatorPink,
-        .top = topBlackBox.getBottom() + separatorPink,
-        .width = yellowBoxWidth + 2 * separatorPink,
-        .height = bottomYellowBox.height + 2 * separatorPink,
-        .colour = COLOUR_LIGHT_GREY,
-    };
-    Box bottomBlackBox = {
-        .left = 0,
-        .top = topBlackBox.getBottom(),
-        .width = topGreyBox.width + 2 * separatorPink,
-        .height = topGreyBox.height + 2 * separatorPink,
-        .cornerSize = 10,
-        .colour = COLOUR_BLACK,
-    };
-    Box gainBox = {
-        .left = midYellowBox.left + separatorBlue + borderSize,
-        .top = midYellowBox.top,
-        .width = 80 * SIZE_FACTOR,
-        .height = midYellowBox.height,
-        .colour = COLOUR_DARK_GREY,
-    };
-    Box toneBox = {
-        .left = gainBox.getRight() + separatorGreen,
-        .top = gainBox.top,
-        .width = gainBox.width,
-        .height = gainBox.height,
-        .colour = COLOUR_DARK_GREY,
-    };
-    Box levelBox = {
-        .left = toneBox.getRight() + separatorGreen,
-        .top = toneBox.top,
-        .width = toneBox.width,
-        .height = toneBox.height,
-        .colour = COLOUR_DARK_GREY,
-    };
-
     paintBox(topBlackBox, g);
-    paintBox(bottomBlackBox, g);
+    //paintBox(bottomBlackBox, g);
     paintBox(topGreyBox, g);
-    paintBox(bottomGreyBox, g);
+    //paintBox(bottomGreyBox, g);
     paintBox(topYellowBox, g);
     paintBox(midYellowBox, g);
-    paintBox(bottomYellowBox, g);
-    paintBox(gainBox, g);
-    paintBox(toneBox, g);
-    paintBox(levelBox, g);
+    //paintBox(bottomYellowBox, g);
+    //paintBox(gainBox, g);
+    //paintBox(toneBox, g);
+    //paintBox(levelBox, g);
 }
 
 void ShittyAmpAudioProcessorEditor::placeKnob(Slider *slider, int column, int row)
@@ -221,6 +192,10 @@ void ShittyAmpAudioProcessorEditor::placeKnob(Slider *slider, int column, int ro
 
 void ShittyAmpAudioProcessorEditor::resized()
 {
+    gainSlider.setBounds(gainBox.top, gainBox.left, gainBox.width, gainBox.height);
+    toneSlider.setBounds(toneBox.top, toneBox.left, toneBox.width, toneBox.height);
+    outLevelSlider.setBounds(levelBox.top, levelBox.left, levelBox.width, levelBox.height);
+
     //Rectangle<int> bounds = getLocalBounds();
 
     // TODO: Add these back
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,22 +1,43 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin editor.
-
-  ==============================================================================
-*/
-
 #pragma once
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 #include "DialLookAndFeel.h"
 
+#define SIZE_FACTOR 1
+#define SEPARATOR_PINK 20
+#define SEPARATOR_GREEN 40
+#define SEPARATOR_BLUE 88
+#define COLOUR_BLACK Colour(28, 28, 28)
+#define COLOUR_YELLOW Colour(249, 226, 85)
+#define COLOUR_LIGHT_GREY Colour(142, 142, 142)
+#define COLOUR_DARK_GREY Colour(58, 58, 58)
+
+
 using SliderAttachment = AudioProcessorValueTreeState::SliderAttachment;
 
-//==============================================================================
-/**
- */
+struct Box {
+    float top;
+    float left;
+    float height;
+    float width;
+    float cornerSize = 0;
+    float borderThickness = 0;
+    Colour colour;
+    Colour borderColour = COLOUR_BLACK;
+
+    float getBottom()
+    {
+        return top + height;
+    }
+    
+    float getRight()
+    {
+        return left + width;
+    }
+};
+
+
 class ShittyAmpAudioProcessorEditor
     : public AudioProcessorEditor
 {
@@ -47,6 +68,17 @@ class ShittyAmpAudioProcessorEditor
     ShittyAmpAudioProcessor &audioProcessor;
 
     DialLookAndFeel dialLookAndFeel;
+
+    Box topYellowBox;
+    Box midYellowBox;
+    Box topGreyBox;
+    Box topBlackBox;
+    Box bottomYellowBox;
+    Box bottomGreyBox;
+    Box bottomBlackBox;
+    Box gainBox;
+    Box toneBox;
+    Box levelBox;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ShittyAmpAudioProcessorEditor)
 };
