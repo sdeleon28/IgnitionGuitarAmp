e86e3b82143464c034f550332ff63901c520fad8
Santiago de Leon <sdeleon28@gmail.com>
2021-02-01 21:12:19 -0300

    Updated installer script

--- Distribution/make-installer.sh
+++ Distribution/make-installer.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 # Inspired on a script from the juce forum -> https://forum.juce.com/t/vst-installer/16654/15
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+JUCERFILE="${DIR}/../IgnitionGuitarAmp.jucer"
 
 shortname="IgnitionGuitarAmp"
 name="com.thmstudio.${shortname}"
-short_version="1.0"
-version="1.0.0"
+
+version=`cat "${JUCERFILE}" | tq 'JUCERPROJECT' -a 'version'`
+short_version=`echo "${version}" | cut -f 1,2 -d'.' -`
 
 function fail {
     printf '%s\n' "$1" >&2  ## Send message to stderr. Exclude >&2 if you don't want it that way.
@@ -51,13 +54,14 @@ sed 's/{version}/'"${version}/g" tmp-1.xml > tmp-2.xml
 sed 's/{au_size}/'"${au_size}/g" tmp-2.xml > tmp-3.xml
 sed 's/{vst3_size}/'"${vst3_size}/g" tmp-3.xml > Distribution.xml
 
+# Make folder for this package
+installer_dir="${DIR}/Builds/${version}"
+mkdir -p "${installer_dir}"
+
 # Package them together to a “thick” installer.
 # Create final product from local assets.
 # productbuild --distribution "./Distribution.xml" --package-path "./" --resources "./Resources" --sign "Developer ID Installer: Audio Damage, Inc." "OSX_${name}_${version}.pkg"
-productbuild --distribution "./Distribution.xml" --package-path "./" --resources "./Resources" "OSX_${name}_${version}.pkg"
-
-# TODO: Remove this
-#exit
+productbuild --distribution "./Distribution.xml" --package-path "./" --resources "./Resources" "${installer_dir}/${shortname}_${version}.pkg"
 
 # Clean up
 rm "${name}.vst3.plist"
