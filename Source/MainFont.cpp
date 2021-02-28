ca9f8a8b5fa58f0469b1dab3a7ceb8183df3af63
Santiago de Leon <sdeleon28@gmail.com>
2021-02-27 23:33:26 -0300

    Select font conditionally based off platform

--- Source/MainFont.cpp
+++ Source/MainFont.cpp
@@ -1,6 +1,21 @@
 #include "MainFont.h"
 
 MainFont::MainFont()
+#ifdef JUCE_MAC
+    : regular(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeCafe_ttf, BinaryData::JorgeLuisCafeCafe_ttfSize)))
+    , lightItalic(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeLightItalic_ttf, BinaryData::JorgeLuisCafeLightItalic_ttfSize)))
+    , light(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeLight_ttf, BinaryData::JorgeLuisCafeLight_ttfSize)))
+    , italic(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeItalic_ttf, BinaryData::JorgeLuisCafeItalic_ttfSize)))
+    , bold(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeBold_ttf, BinaryData::JorgeLuisCafeBold_ttfSize)))
+    , boldItalic(Font(
+          Typeface::createSystemTypefaceFor(BinaryData::JorgeLuisCafeBoldItalic_ttf, BinaryData::JorgeLuisCafeBoldItalic_ttfSize)))
+#endif
+#ifdef JUCE_WINDOWS
     : regular(Font(
           Typeface::createSystemTypefaceFor(BinaryData::OpenSansRegular_ttf, BinaryData::OpenSansRegular_ttfSize)))
     , lightItalic(Font(
@@ -13,6 +28,7 @@ MainFont::MainFont()
           Typeface::createSystemTypefaceFor(BinaryData::OpenSansBold_ttf, BinaryData::OpenSansBold_ttfSize)))
     , boldItalic(Font(
           Typeface::createSystemTypefaceFor(BinaryData::OpenSansBoldItalic_ttf, BinaryData::OpenSansBoldItalic_ttfSize)))
+#endif
 {
 }
 
