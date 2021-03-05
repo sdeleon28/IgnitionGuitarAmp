a3a6cf08988c570c1b115e1cef0230e5e1bfc8e9
Santiago de Leon <sdeleon28@gmail.com>
2021-03-04 21:41:54 -0300

    Updated version in JUCE files

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -49,7 +49,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "IgnitionGuitarAmp";
     const char* const  companyName    = "thmstudio";
-    const char* const  versionString  = "0.0.1";
-    const int          versionNumber  = 0x1;
+    const char* const  versionString  = "1.0.0";
+    const int          versionNumber  = 0x10000;
 }
 #endif
